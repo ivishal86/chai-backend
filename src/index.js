d25ba6974d406f9a648afa272baa0780a2188241
Vishal Gwala <118927776+ivishal86@users.noreply.github.com>
2024-03-12 15:09:08 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
-import dotenv from 'dotenv'
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+// import { DB_NAME } from './constants.js';
 import connectDB from "./db/index.js";
+import {app} from './app.js'
+// import express from "express";
+// import mongoose from "mongoose";
 
 dotenv.config({
-    path:'.env'
+    path: './.env'
+});
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
 
-connectDB();
+
+
+// const app = express();
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        
+//         // Error handler for the app
+//         app.use((err, req, res, next) => {
+//             console.error("ERROR: ", err);
+//             res.status(500).send('Something broke!');
+//         });
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         });
+
+//     } catch (error) {
+//         console.error("ERROR: ", error);
+//         throw error;
+//     }
+// })();
